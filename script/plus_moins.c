522008f3c84eaeff4b7b078676369ade2fe151ab
KhertishL <109499710+KhertishL@users.noreply.github.com>
2022-07-24 23:39:48 +0400

    Added difficulty and prompt message.

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -22,21 +22,67 @@ int plus_moins(int choix_joueur, int valeur_a_trouver)
     }
 }
 
-int main()
+int game(int valeur_a_trouver, int choix_joueur)
 {
-    int valeur_a_trouver;
-    int choix_joueur;
-    
-    valeur_a_trouver = rand() % 100 + 1;
+   valeur_a_trouver = rand() % 100 + 1;
     printf("Essayez de deviner la valeur: ");
     scanf("%d",&choix_joueur);
     plus_moins(choix_joueur, valeur_a_trouver);
     
-    while(choix_joueur != valeur_a_trouver)
-    {
-        printf("Essayez de deviner la valeur: ");
-        scanf("%d",&choix_joueur);
-        plus_moins(choix_joueur, valeur_a_trouver);
+}
+
+int main()
+{
+    int valeur_a_trouver;
+    int choix_joueur;
+    int difficulty;
+    int count;
+    
+    printf("Choisissez une difficulter 1,2 ou 3\n1.facile(essaie illimites)\n2.moyen(25 essaies)\n3.difficile(10 essaies)\n");
+    printf("Difficulter : ");
+    scanf("%d",&difficulty);
+    
+    if(difficulty == 1){
+        
+        game(valeur_a_trouver, choix_joueur);
+        while(choix_joueur != valeur_a_trouver)
+            {
+                printf("Essayez de deviner la valeur: ");
+                scanf("%d",&choix_joueur);
+                plus_moins(choix_joueur, valeur_a_trouver);
+            } 
+    }
+    
+    if(difficulty == 2){
+        
+        count = 1;
+        game(valeur_a_trouver, choix_joueur);
+        while(choix_joueur != valeur_a_trouver && count < 25)
+            {
+                printf("Essayez de deviner la valeur: ");
+                scanf("%d",&choix_joueur);
+                plus_moins(choix_joueur, valeur_a_trouver);
+                count++;
+                if(count == 25){
+                    printf("Vous avez perdu.");
+                }
+            } 
+    }
+    
+    if(difficulty == 3){
+        
+        count = 1;
+        game(valeur_a_trouver, choix_joueur);
+        while(choix_joueur != valeur_a_trouver && count < 10)
+            {
+                printf("Essayez de deviner la valeur: ");
+                scanf("%d",&choix_joueur);
+                plus_moins(choix_joueur, valeur_a_trouver);
+                count++;
+                if(count == 10){
+                    printf("Vous avez perdu.");
+                }
+            } 
     }
     
 }
